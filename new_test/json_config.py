d3d68475931254f85bfe78ff6a9096bda7860110
austinmeier <meiera@science.oregonstate.edu>
2016-06-09 16:33:15 -0700

    works now with evidence codes in the trait map file.

---
+++ new_test/json_config.py
@@ -0,0 +1,20 @@
+import json
+import urllib2
+
+
+
+
+
+data = {
+    'name' : 'AUSTIN',
+    'age': 30,
+    'sex': True
+}
+
+json_str = json.dumps(data)
+
+# for thing in json_str:
+#     print type(thing)
+#     print thing
+
+test.json =
